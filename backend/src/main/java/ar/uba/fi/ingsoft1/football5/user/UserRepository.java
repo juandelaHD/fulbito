5b00cd65850e6280d4d9077fd50dfccc54e3faff
Juan Martin de la Cruz <jdelacruz@fi.uba.ar>
2025-05-26 21:18:06 -0300

    feat: implement email confirmation feature with token generation and verification

--- backend/src/main/java/ar/uba/fi/ingsoft1/football5/user/UserRepository.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/football5/user/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    Optional<User> findByEmailConfirmationToken(String token);
+
 }
