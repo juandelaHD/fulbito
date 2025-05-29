8028d8afa5086e544a304cce46a3551ce38ff362
Abril Giordano Hoyo <agiordano@fi.uba.ar>
2025-05-29 19:26:43 +0000

    Revert "Merge branch 'FUT-77-Field-Creation-Frontend' into 'main'"
    This reverts merge request !10

--- backend/src/main/java/ar/uba/fi/ingsoft1/football5/config/security/JwtUserDetails.java
+++
@@ -1,6 +0,0 @@
-package ar.uba.fi.ingsoft1.football5.config.security;
-
-public record JwtUserDetails (
-        String username,
-        String role
-) {}
--- backend/src/main/java/ar/uba/fi/ingsoft1/football5/user/UserCredentials.java
+++
@@ -1,6 +0,0 @@
-package ar.uba.fi.ingsoft1.football5.user;
-
-public interface UserCredentials {
-    String getUsername();
-    String getPassword();
-}
