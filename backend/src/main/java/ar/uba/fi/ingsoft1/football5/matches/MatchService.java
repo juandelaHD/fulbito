40f679d465a57c1761371c0954187653126f48b4
Juan Martin de la Cruz <jdelacruz@fi.uba.ar>
2025-05-31 20:25:54 -0300

    feat: integrate email confirmation for match creation with UUID token

--- backend/src/main/java/ar/uba/fi/ingsoft1/football5/matches/MatchService.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/football5/matches/MatchService.java
@@ -8,10 +8,12 @@ import ar.uba.fi.ingsoft1.football5.user.User;
 import ar.uba.fi.ingsoft1.football5.user.UserDTO;
 import ar.uba.fi.ingsoft1.football5.user.UserService;
 
+import ar.uba.fi.ingsoft1.football5.user.email.EmailSenderService;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -20,15 +22,18 @@ public class MatchService {
     private final MatchRepository matchRepository;
     private final UserService userService;
     private final FieldService fieldService;
+    private final EmailSenderService emailSenderService;
 
     public MatchService(
             MatchRepository matchRepository,
             UserService userService,
-            FieldService fieldService
+            FieldService fieldService,
+            EmailSenderService emailSenderService
     ) {
         this.matchRepository = matchRepository;
         this.userService = userService;
         this.fieldService = fieldService;
+        this.emailSenderService = emailSenderService;
     }
 
     public Match loadMatchById(Long id) throws ItemNotFoundException {
@@ -90,16 +95,23 @@ public class MatchService {
         // Agregar el organizador a los jugadores del partido
         newMatch.addPlayer(organizerUser);
 
-        // Enviar notificación al organizador y crear el UUID (token) de confirmación por email
-        // emailService.sendMatchCreatedNotification(organizerUser, newMatch);
-        // newMatch.setEmailConfirmationToken(UUID.randomUUID().toString());
-        // newMatch.setConfirmationSent(true);
+        // Enviar y crear el UUID (token) de confirmación por email
+        String token = UUID.randomUUID().toString();
+        emailSenderService.sendMailToVerifyMatch(
+                organizerUser.getUsername(),
+                token,
+                match.date(),
+                match.startTime(),
+                match.endTime()
+        );
+        newMatch.setEmailConfirmationToken(token);
+        newMatch.setConfirmationSent(true);
 
+        // Guardar el partido
         Match savedMatch = matchRepository.save(newMatch); // Guardar de nuevo para asegurar que se actualiza la lista de jugadores
 
         // Retornar el DTO del partido guardado
         return new MatchDTO(savedMatch);
     }
-
 
 }
