f9ad2e92a0c2ca83b67d351e01517b2b90435592
Abril Giordano Hoyo <abril.giordano@despegar.com>
2025-06-14 16:24:31 -0300

    feat: add asynchronous email sending methods for various notifications

--- backend/src/main/java/ar/uba/fi/ingsoft1/football5/Football5Application.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/football5/Football5Application.java
@@ -2,8 +2,10 @@ package ar.uba.fi.ingsoft1.football5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Football5Application {
 
 	public static void main(String[] args) {
--- backend/src/main/java/ar/uba/fi/ingsoft1/football5/user/email/EmailSenderService.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/football5/user/email/EmailSenderService.java
@@ -3,6 +3,7 @@ package ar.uba.fi.ingsoft1.football5.user.email;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -20,56 +21,66 @@ public class EmailSenderService {
         this.senderEmailAddress = senderEmailAddress;
     }
 
-    public void sendMail(String recipientEmailAddress, EmailWriter mailType) {
+    @Async
+    public void sendMailToVerifyAccount(String recipientEmailAddress, String token) {
+        this.sendMail(recipientEmailAddress, new AccountVerificationMailWriter(token));
+    }
+
+    @Async
+    public void sendPasswordResetMail(String recipientEmailAddress, String token) {
+        this.sendMail(recipientEmailAddress, new PasswordResetMailWriter(token));
+    }
+
+    @Async
+    public void sendPasswordChangedMail(String recipientEmailAddress) {
+        this.sendMail(recipientEmailAddress, new PasswordChangeMailWriter());
+    }
+
+    @Async
+    public void sendReservationMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
+        this.sendMail(recipientEmail, new MatchReservationMailWriter(date, start, end));
+    }
+
+    @Async
+    public void sendMatchNewReservationMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end, String fieldName) {
+        this.sendMail(recipientEmail, new MatchNewReservationMailWriter(date, start, end, fieldName));
+    }
+
+    @Async
+    public void sendTeamCaptainMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end, String OrganizerUsername) {
+        this.sendMail(recipientEmail, new TeamCaptainMatchMailWriter(date, start, end, OrganizerUsername));
+    }
+
+    @Async
+    public void sendTeamAssignmentMail(String recipientEmail, String teamName, LocalDate date, LocalDateTime start, LocalDateTime end) {
+        this.sendMail(recipientEmail, new TeamAssignmentMailWriter(teamName, date, start, end));
+    }
+
+    @Async
+    public void sendUnsubscribeMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
+        this.sendMail(recipientEmail, new UnsubscribeMailWriter(date, start, end));
+    }
+
+    @Async
+    public void sendReservationConfirmedMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
+        this.sendMail(recipientEmail, new MatchReservationConfirmedMailWriter(date, start, end));
+    }
+
+    @Async
+    public void sendMatchFinishedMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
+        this.sendMail(recipientEmail, new MatchFinishedMailWriter(date, start, end));
+    }
+
+    @Async
+    public void sendMatchCancelledMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
+        this.sendMail(recipientEmail, new MatchCancelledMailWriter(date, start, end));
+    }
+
+    private void sendMail(String recipientEmailAddress, EmailWriter mailType) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(this.senderEmailAddress);
         message.setTo(recipientEmailAddress);
         mailType.writeMessage(message);
         mailSender.send(message);
     }
-
-    public void sendMailToVerifyAccount(String recipientEmailAddress, String token) {
-        this.sendMail(recipientEmailAddress, new AccountVerificationMailWriter(token));
-    }
-
-    public void sendPasswordResetMail(String recipientEmailAddress, String token) {
-        this.sendMail(recipientEmailAddress, new PasswordResetMailWriter(token));
-    }
-
-    public void sendPasswordChangedMail(String recipientEmailAddress) {
-        this.sendMail(recipientEmailAddress, new PasswordChangeMailWriter());
-    }
-
-    public void sendReservationMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
-        this.sendMail(recipientEmail, new MatchReservationMailWriter(date, start, end));
-    }
-
-    public void sendMatchNewReservationMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end, String fieldName) {
-        this.sendMail(recipientEmail, new MatchNewReservationMailWriter(date, start, end, fieldName));
-    }
-
-    public void sendTeamCaptainMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end, String OrganizerUsername) {
-        this.sendMail(recipientEmail, new TeamCaptainMatchMailWriter(date, start, end, OrganizerUsername));
-    }
-
-    public void sendTeamAssignmentMail(String recipientEmail, String teamName, LocalDate date, LocalDateTime start, LocalDateTime end) {
-        this.sendMail(recipientEmail, new TeamAssignmentMailWriter(teamName, date, start, end));
-    }
-
-    public void sendUnsubscribeMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
-        this.sendMail(recipientEmail, new UnsubscribeMailWriter(date, start, end));
-    }
-
-    public void sendReservationConfirmedMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
-        this.sendMail(recipientEmail, new MatchReservationConfirmedMailWriter(date, start, end));
-    }
-
-    public void sendMatchFinishedMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
-        this.sendMail(recipientEmail, new MatchFinishedMailWriter(date, start, end));
-    }
-
-    public void sendMatchCancelledMail(String recipientEmail, LocalDate date, LocalDateTime start, LocalDateTime end) {
-        this.sendMail(recipientEmail, new MatchCancelledMailWriter(date, start, end));
-    }
-
 }
