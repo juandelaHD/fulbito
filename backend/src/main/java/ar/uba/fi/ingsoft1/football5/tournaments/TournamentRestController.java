b9024f7670a2e3f8f7dfc3fd66380820c64a774f
emanu2345 <vagrant@sisop>
2025-06-18 19:48:56 +0000

    Added get to organized tournaments by the user and to filter for an specific user. Now gives teams registered in match

--- backend/src/main/java/ar/uba/fi/ingsoft1/football5/tournaments/TournamentRestController.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/football5/tournaments/TournamentRestController.java
@@ -53,6 +53,20 @@ public class TournamentRestController {
         return tournamentService.getTournamentsFiltered(organizerUsername, openForRegistration);
     }
 
+    @GetMapping(path = "/organized-Tournaments", produces = "application/json")
+    @ApiResponse(responseCode = "200", description = "List of tournaments retrieved successfully")
+    public List<TournamentResponseDTO> getTournamentsOrganized(            
+        @AuthenticationPrincipal JwtUserDetails userDetails) {
+        return tournamentService.getTournamentsOrganized(userDetails);
+    }
+
+    @GetMapping(path = "/organized-Tournaments", produces = "application/json")
+    @ApiResponse(responseCode = "200", description = "List of tournaments retrieved successfully")
+    public List<TournamentResponseDTO> getTournamentsOrganizedBy(            
+        @RequestParam String organizerUsername) {
+        return tournamentService.getTournamentsOrganizedBy(organizerUsername);
+    }
+
     @PutMapping("/{id}")
     @Operation(summary = "Update an existing tournament")
     @ApiResponses(value = {
